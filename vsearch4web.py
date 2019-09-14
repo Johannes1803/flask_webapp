4057b598b29c37fecdc5721bc144803c1a3d44d6
Johannes_1 <Johannes.Birk11@web.de>
2019-09-14 12:15:52 +0200

    pep8  formatting

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request
+from flask import Flask, render_template, request
 from vsearch import search_4_letters
 
 app = Flask(__name__)
@@ -9,16 +9,19 @@ def do_search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search_4_letters(phrase, letters))
-    return render_template( 'results.html',
-                            the_title='Your search results!',
-                            the_phrase=phrase,
-                            the_letters=letters,
-                            the_results=results,)
+    return render_template(
+                    'results.html', the_title='Your search results!',
+                    the_phrase=phrase,
+                    the_letters=letters,
+                    the_results=results,)
+
 
 @app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html',
-                            the_title='Welcome to search 4 letters on the web!')
+    return render_template(
+                        'entry.html',
+                        the_title='Welcome to search 4 letters on the web!')
+
 
 app.run(debug=True)
