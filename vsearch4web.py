e6824adcfce48eb2e075bab57a489bc53d50985d
Johannes_1 <Johannes.Birk11@web.de>
2019-09-14 11:48:00 +0200

    render results more nicely

--- vsearch4web.py
+++ vsearch4web.py
@@ -11,7 +11,12 @@ def hello() -> str:
 def do_search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search_4_letters(phrase, letters))
+    results = str(search_4_letters(phrase, letters))
+    return render_template( 'results.html',
+                            the_title='Your search results!',
+                            the_phrase=phrase,
+                            the_letters=letters,
+                            the_results=results)
 
 
 @app.route('/entry')
