bd14a7bbf5bb3fc9f7d7b7f1811f96c4668c25ad
Johannes_1 <Johannes.Birk11@web.de>
2019-09-17 19:28:18 +0200

    remove pep 8 violations

--- vsearch4web.py
+++ vsearch4web.py
@@ -18,18 +18,17 @@ def log_request(req: 'flask_request', res: str) -> None:
     conn = mysql.connector.connect(**dbconfig)
     cursor = conn.cursor()
 
-    _SQl = """
+    _SQL = """
             insert into log
             (phrase, letters, ip, browser_string, results)
             values
             (%s, %s, %s, %s, %s)"""
-    cursor.execute(
-        _SQl, (req.form['phrase'],
-        req.form['letters'],
-        req.remote_addr,
-        req.user_agent.browser,
-        res, )
-    )
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res, ))
+
     conn.commit()
     cursor.close()
     conn.close()
@@ -48,11 +47,14 @@ def do_search() -> 'html':
     letters = request.form['letters']
     results = str(search_4_letters(phrase, letters))
     log_request(request, results)
-    return render_template(
-                    'results.html', the_title=title,
-                    the_phrase=phrase,
-                    the_letters=letters,
-                    the_results=results,)
+    rendered = render_template(
+        'results.html',
+        the_title=title,
+        the_phrase=phrase,
+        the_letters=letters,
+        the_results=results)
+
+    return rendered
 
 
 @app.route('/')
@@ -63,8 +65,8 @@ def entry_page() -> 'html':
     Provide form element for user to enter search request.
     """
     return render_template(
-                        'entry.html',
-                        the_title='Welcome to search 4 letters on the web!')
+        'entry.html',
+        the_title='Welcome to search 4 letters on the web!')
 
 
 @app.route('/viewlog')
@@ -78,11 +80,10 @@ def view_the_log() -> 'html':
                 contents[-1].append(escape(item))
     titles = ('FormData', 'Remote_addr', 'User_agent', 'Results')
     return render_template(
-                    'viewlog.html',
-                    the_title='View Log',
-                    the_row_titles=titles,
-                    the_data=contents
-                    )
+        'viewlog.html',
+        the_title='View Log',
+        the_row_titles=titles,
+        the_data=contents)
 
 
 if __name__ == '__main__':
